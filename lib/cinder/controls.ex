ef720bd4e90abbcee9bbdde6f5a0c1d011487654
Kven <200dur@mail.ru>
2026-08-02 12:12:04 +0300

    Fix clearing a filter that holds a partially entered date or number

--- lib/cinder/controls.ex
+++ lib/cinder/controls.ex
@@ -317,6 +317,8 @@ defmodule Cinder.Controls do
       <button
         :if={@has_filters}
         type="button"
+        id={"#{@table_id}-clear-all"}
+        phx-hook={FilterManager.clear_inputs_hook_name()}
         phx-click="clear_all_filters"
         phx-target={@target}
         class={[@theme.filter_clear_all_class, if(@active_filter_count == 0, do: "invisible", else: "")]}
